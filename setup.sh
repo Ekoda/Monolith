bf8199dea8838bac4d384589e2cf5ae2321ec696
pontusblomqvist <pontus.blomqvist94@gmail.com>
2023-10-06 22:17:44 +0200

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 npm install
+grep -q "db-url-here" .env && \
+{ sed 's|db-url-here|file:./dev.db|g' .env > temp; mv temp .env; } || \
+echo "Placeholder not found"
 npx prisma migrate dev --name init
-sed -i 's|db-url-here|file:./dev.db|g' .env
