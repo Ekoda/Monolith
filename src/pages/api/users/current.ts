80b752f2108327704e9fb9a4e6ff15adce84d9fd
pontusblomqvist <pontus.blomqvist94@gmail.com>
2023-10-06 22:55:12 +0200

    email based auth (cleanup)

--- src/pages/api/users/current.ts
+++ src/pages/api/users/current.ts
@@ -2,7 +2,6 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {internalError500, methodNotAllowed405, ok200, or403, or404} from "@/utils/apiUtils";
 import {userSignedIn} from "@/utils/authUtils";
 import {findUserByEmail} from "@/backend/services/userService";
-import {validatePresent} from "@/utils/errorUtils";
 
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
