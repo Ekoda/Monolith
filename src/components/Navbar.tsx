3d795cfc9d9f7be9abc30efc70c6dc0c2f6d094b
pontusblomqvist <pontus.blomqvist94@gmail.com>
2023-10-06 13:10:01 +0200

    fixed navbar for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import {
     AppBar, Avatar, Backdrop, Box, Button, CircularProgress, Container,
-    IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography
+    IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography, Link
 } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import {signOut, useSession} from 'next-auth/react';
 import {TITLE} from "@/config/constants";
 
@@ -54,6 +55,51 @@ const Navbar = () => {
             <AppBar component={'nav'} variant={"outlined"} elevation={0} sx={{backgroundColor: "transparent", position: "static"}}>
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="menu"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page.name}>
+                                        <Link  underline={"none"}
+                                               color={"secondary"}
+                                               href={page.path}
+                                               sx={{
+                                                   color: 'inherit',
+                                                   '&:visited': {
+                                                       color: 'inherit'
+                                                   }
+                                               }}
+                                        >{page.name}</Link>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
                         <Typography
                             variant="h6"
                             noWrap
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${inter.className}`}>
         <p>Welcome to the index page</p>
       </main>
     </>
