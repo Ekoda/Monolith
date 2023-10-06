c539f97aca1e44181077e15aaf7a279227ff6caf
pontusblomqvist <pontus.blomqvist94@gmail.com>
2023-10-07 00:07:43 +0200

    improved authUtils naming

--- src/pages/api/users/current.ts
+++ src/pages/api/users/current.ts
@@ -1,16 +1,16 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {internalError500, methodNotAllowed405, ok200, or403, or404} from "@/utils/apiUtils";
-import {getUser} from "@/utils/authUtils";
+import {getSessionUser} from "@/utils/authUtils";
 import {findUserByEmail} from "@/backend/services/userService";
 
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    const user = await getUser(req);
-    or403(res, !!user, "You must be signed in to view this")
+    const sessionUser = await getSessionUser(req);
+    or403(res, !!sessionUser, "You must be signed in to view this")
 
     switch (req.method) {
         case "GET":
-            return findUserByEmail(user?.email as string)
+            return findUserByEmail(sessionUser?.email as string)
                 .then(user => or404(res, user))
                 .then(user => ok200(res, user))
                 .catch(error => internalError500(res, error.message));
--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -1,5 +1,4 @@
-import {getServerSession} from "next-auth";
-import {NextApiRequest, NextApiResponse} from "next";
+import {NextApiRequest} from "next";
 import {getSession} from "next-auth/react";
 
 interface SessionUser {
@@ -8,7 +7,7 @@ interface SessionUser {
     image?: string | null | undefined
 }
 
-export async function getUser(req: NextApiRequest): Promise<SessionUser | null> {
+export async function getSessionUser(req: NextApiRequest): Promise<SessionUser | null> {
     const session = await getSession({req});
     if (session?.user?.email) {
         return session.user;
@@ -16,7 +15,7 @@ export async function getUser(req: NextApiRequest): Promise<SessionUser | null> 
     return null;
 }
 
-export async function userSignedIn(req: NextApiRequest): Promise<boolean> {
-    const user = await getUser(req);
+export async function isSignedIn(req: NextApiRequest): Promise<boolean> {
+    const user = await getSessionUser(req);
     return !!user;
 }
