55c6d4bd98b614e5dd6e381896620114d96c0cc9
pontusblomqvist <pontus.blomqvist94@gmail.com>
2023-10-06 20:06:21 +0200

    additional errorUtils and tests

--- src/tests/errorUtils.test.ts
+++ src/tests/errorUtils.test.ts
@@ -1,27 +1,77 @@
-import {describe, expect, test} from "@jest/globals";
-import {validatePresent} from "@/utils/errorUtils";
+import {describe, expect, test, jest} from "@jest/globals";
+import {assert, checkType, retry, validatePresent} from "@/utils/errorUtils";
 
-describe('validatePresent function', () => {
-    // Test that a value passes through correctly
-    test('should return the value when it is not null or undefined', () => {
-        const value = 42;
-        const result = validatePresent(value);
-        expect(result).toBe(value);
+
+describe('errorUtils', () => {
+    describe('validatePresent function', () => {
+        test('should return the value when it is not null or undefined', () => {
+            const value = 42;
+            const result = validatePresent(value);
+            expect(result).toBe(value);
+        });
+
+        test('should throw an error when value is null', () => {
+            expect(() => validatePresent(null)).toThrowError('Value is null or undefined');
+        });
+
+        test('should throw an error when value is undefined', () => {
+            expect(() => validatePresent(undefined)).toThrowError('Value is null or undefined');
+        });
+
+        test('should throw a custom error message when provided', () => {
+            const customMessage = 'Custom Error Message';
+            expect(() => validatePresent(null, customMessage)).toThrowError(customMessage);
+        });
     });
 
-    // Test that null throws an error
-    test('should throw an error when value is null', () => {
-        expect(() => validatePresent(null)).toThrowError('Value is null or undefined');
+    describe('assert function', () => {
+        test('should not throw an error when condition is true', () => {
+            expect(() => assert(true)).not.toThrow();
+        });
+
+        test('should throw an error when condition is false', () => {
+            expect(() => assert(false)).toThrowError('Assertion failed');
+        });
+
+        test('should throw a custom error message when provided', () => {
+            const customMessage = 'Custom Assertion Error Message';
+            expect(() => assert(false, customMessage)).toThrowError(customMessage);
+        });
     });
 
-    // Test that undefined throws an error
-    test('should throw an error when value is undefined', () => {
-        expect(() => validatePresent(undefined)).toThrowError('Value is null or undefined');
+    describe('checkType function', () => {
+        test('should return true when types match', () => {
+            expect(checkType<number>(42, 'number')).toBe(true);
+        });
+
+        test('should throw an error when types do not match', () => {
+            expect(() => checkType<number>('42', 'number')).toThrowError('Expected number, got string');
+        });
+
+        test('should throw a custom error message when provided', () => {
+            const customMessage = 'Custom Type Error Message';
+            expect(() => checkType<number>('42', 'number', customMessage)).toThrowError(customMessage);
+        });
     });
 
-    // Test custom error message
-    test('should throw a custom error message when provided', () => {
-        const customMessage = 'Custom Error Message';
-        expect(() => validatePresent(null, customMessage)).toThrowError(customMessage);
+    describe('retry function', () => {
+        test('should return the result when the function succeeds', async () => {
+            const mockFn = jest.fn(() => Promise.resolve(42));
+            const result = await retry(mockFn);
+            expect(result).toBe(42);
+        });
+
+        test('should retry the function the specified number of times on failure', async () => {
+            const mockFn = jest.fn(() => Promise.reject(new Error('Temporary failure')));
+            await expect(retry(mockFn, 3)).rejects.toThrowError('All 3 retry attempts failed');
+            expect(mockFn).toHaveBeenCalledTimes(3);
+        });
+
+        test('should throw the last error when all retries fail', async () => {
+            const mockFn = jest.fn(() => Promise.reject(new Error('Temporary failure')));
+            await expect(retry(mockFn)).rejects.toThrowError('Last error: Error: Temporary failure');
+        });
     });
+
+
 });
--- src/utils/errorUtils.ts
+++ src/utils/errorUtils.ts
@@ -1,4 +1,29 @@
 
+export function assert(condition: boolean, errorMessage?: string): asserts condition {
+    if (!condition) {
+        throw new Error(errorMessage || "Assertion failed");
+    }
+}
+
+export function checkType<T>(value: unknown, type: string, errorMessage?: string): value is T {
+    if (typeof value !== type) {
+        throw new Error(errorMessage || `Expected ${type}, got ${typeof value}`);
+    }
+    return true;
+}
+
+export async function retry<T>(fn: () => Promise<T>, retries: number = 3): Promise<T> {
+    let lastError;
+    for (let i = 0; i < retries; i++) {
+        try {
+            return await fn();
+        } catch (error) {
+            lastError = error;
+        }
+    }
+    throw new Error(`All ${retries} retry attempts failed. Last error: ${lastError}`);
+}
+
 export function validatePresent<T>(value: T | null | undefined, errorMessage?: string): T {
     if (value === null || value === undefined) {
         throw new Error(errorMessage || "Value is null or undefined");
