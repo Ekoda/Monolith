bcbf03e43b10c74e1cd4b448c2cdd259aed2b9cf
Pontus Blomqvist <68470513+Ekoda@users.noreply.github.com>
2023-10-09 16:34:48 +0200

    smoll mock fix

--- src/mocks/users.ts
+++ src/mocks/users.ts
@@ -7,7 +7,7 @@ const users: User[] = [
         email: "john.doe@example.com",
         createdAt: new Date("2023-01-20T00:00:00Z"),
         updatedAt: new Date("2023-01-20T00:00:00Z"),
-        lastLogin: new Date("2023-01-20T00:00:00Z")
+        lastLoginAt: new Date("2023-01-20T00:00:00Z")
     },
     {
         id: "2",
@@ -15,6 +15,6 @@ const users: User[] = [
         email: "jane.smith@example.com",
         createdAt: new Date("2023-01-21T00:00:00Z"),
         updatedAt: new Date("2023-01-21T00:00:00Z"),
-        lastLogin: new Date("2023-01-21T00:00:00Z")
+        lastLoginAt: new Date("2023-01-21T00:00:00Z")
     },
 ];
