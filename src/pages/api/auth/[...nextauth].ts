aa75a11195a73d2009852784f55ed35f188733d3
pontusblomqvist <pontus.blomqvist94@gmail.com>
2023-10-14 23:18:52 +0200

    nextauth setup

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import {handleSignIn} from "@/backend/controllers/authController";
 
 export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID as string,
